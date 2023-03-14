2fd6db3b8811837fb8ce50f940fd0bfd176c70ec
laulpogan <paul@zaibatsuheavy.industries>
2023-03-13 22:54:20 -0700

    changes

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,9 @@ import { APIOutput } from './types';
 const app = express();
 
 const port = Number(process.env.PORT || 8080);
+const SERVER_URL = process.env.SERVER_URL;
 
-if (process.env.REDISTOGO_URL) {
-  var rtg = require('url').parse(process.env.REDISTOGO_URL);
-  var redis = require('redis').createClient(rtg.port, rtg.hostname);
 
-  redis.auth(rtg.auth.split(':')[1]);
-} else {
-  var redis = require('redis').createClient();
-}
-
-const limiter = require('express-limiter')(app, redis);
-
-limiter({
-  path: '/v2',
-  method: 'get',
-  lookup: ['connection.remoteAddress'],
-  // 300 requests per minute
-  total: 300,
-  expire: 1000 * 60,
-});
 
 const sendResponse = (res: Response, output: APIOutput | null) => {
   if (!output) {
@@ -68,7 +51,7 @@ app.get('/v2', async (req, res) => {
         .json({ error: 'Invalid URL' });
     }
 
-    url = url.indexOf('://') === -1 ? 'http://' + url : url;
+    url = (url.indexOf('://') === -1 )? 'http://' + url : url;
 
     const isUrlValid =
       /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi.test(
@@ -112,7 +95,7 @@ app.get('/v2', async (req, res) => {
         ? og.description
         : meta.description
         ? meta.description
-        : null;
+        : "";
       const title = (og.title ? og.title : meta.title) || '';
       const siteName = og.site_name || '';
 
